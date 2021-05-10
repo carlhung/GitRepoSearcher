4126122a39b250101af664ab05f6f1fedb4f70bb
carlhung <carl.hung@yahoo.com>
2021-05-10 14:13:22 +0100

    added unit test

--- GitRepoSearcherTests/GitRepoSearcherTests.swift
+++ GitRepoSearcherTests/GitRepoSearcherTests.swift
@@ -6,21 +6,27 @@
 //
 
 import XCTest
-import GitRepoSearcher
+@testable import GitRepoSearcher
 
 class GitRepoSearcherTests: XCTestCase {
-
+    
+    var itemArray: [Items]?
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        itemArray = []
+        
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        XCTAssert(itemArray?.count == 0, "It should be 0.")
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(itemArray != nil, "itemArray is nil. But it shouldn't be.")
     }
 
     func testPerformanceExample() throws {
