11fcd232c05f81294fd0f4d97e3631795c5f6aba
carlhung <carl.hung@yahoo.com>
2021-05-10 12:39:18 +0100

    finished detail view

--- GitRepoSearcher/VC/RepoDetailViewController.swift
+++ GitRepoSearcher/VC/RepoDetailViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import WebKit
 
 class RepoDetailViewController: UIViewController {
 
     // MARK: - Properties
     private let repo: Items
+    private var webViewY: CGFloat!
+    private let webView = WKWebView()
+    
     
     // MARK: - Init
     init(repo: Items) {
@@ -27,15 +31,50 @@ class RepoDetailViewController: UIViewController {
     // MARK: - Override
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.view.backgroundColor = .green
-//        let height = self.navigationController?.navigationBar.frame.maxY
-//        let dualLabel = LeftRightLabelView(left: "left", right: "right", size: CGSize(width: self.safeAreaFrame.width, height: 20))
-//        dualLabel.frame.origin = CGPoint(x: self.safeAreaFrame.origin.x, y: height ?? 0)//self.safeAreaFrame.origin.y + (height ?? 0))
-//        self.view.addSubview(dualLabel)
-        // Do any additional setup after loading the view.
+        self.setup()
     }
     
+    // MARK: - Setup
+    func setup() {
+        self.setupSelf()
+        self.setupNavigationBar()
+        self.setupLabels()
+        self.setupWebView()
+    }
+    
+    func setupSelf() {
+        self.view.backgroundColor = .white
+    }
+    
+    func setupNavigationBar() {
+        self.title = self.repo.name
+    }
+    
+    func setupLabels() {
+        let height = self.navigationController?.navigationBar.frame.maxY ?? 0
+        let additionalSpace: CGFloat = 2
+        let startY = height + additionalSpace
+        let dualLabelSize = CGSize(width: self.safeAreaFrame.width, height: 20)
+        let firstDualLabel = LeftRightLabelView(left: "forks: \(repo.forks.stringValue)", right: "issues: \(repo.open_issues.stringValue)", size: dualLabelSize)
+        let secondDualLabel = LeftRightLabelView(left: "watchers: \(repo.watchers.stringValue)", right: "size: \(repo.size.stringValue)", size: dualLabelSize)
+        let thirdualLabel = LeftRightLabelView(left: "description: \(repo.description ?? "N/A")", right: "default branch: \(repo.default_branch ?? "N/A")", size: dualLabelSize)
 
+        webViewY = [firstDualLabel, secondDualLabel, thirdualLabel].reduce(startY) { result, label in
+            label.frame.origin = CGPoint(x: self.safeAreaFrame.origin.x, y: result)
+            self.view.addSubview(label)
+            return  result + label.frame.height + additionalSpace
+        }
+    }
+    
+    func setupWebView() {
+        guard let repoUrl = repo.html_url, let branch = repo.default_branch else { return }
+        let urlStr = repoUrl + "/blob/" + branch + "/README.md"
+        guard let url = URL(string: urlStr) else { return }
+        self.webView.frame = CGRect(x: self.safeAreaFrame.origin.x, y: self.webViewY, width: self.safeAreaFrame.width, height: self.view.frame.height - self.webViewY)
+        self.view.addSubview(self.webView)
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
     /*
     // MARK: - Navigation
 
@@ -46,4 +85,14 @@ class RepoDetailViewController: UIViewController {
     }
     */
 
+}
+
+fileprivate extension Optional where Wrapped == Int {
+    var stringValue: String {
+        if let val = self {
+            return String(val)
+        } else {
+            return "N/A"
+        }
+    }
 }
